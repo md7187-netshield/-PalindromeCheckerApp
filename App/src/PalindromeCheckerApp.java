42407664a4ba15c5f564e864ec210258d678b342
md7187-netshield <md7187@srmist.edu.in>
2026-03-10 15:22:39 +0530

    mahasreerajhaan added CharacterArrayBasedPalindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,18 +1,15 @@
-public class PalindromeCheckerApp {
+void main() {
 
+    String input = "madam";
+    StringBuilder reversed = new StringBuilder();
 
-    public static void main(String[] args) {
-
-        String input = "madam";
-        String reversed = "";
-
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
-        }
-
-        boolean isPalindrome = input.equals(reversed);
-
-        System.out.println("Input text: " + input);
-        System.out.println("Reversed text: " + reversed);
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    for (int i = input.length() - 1; i >= 0; i--) {
+        reversed.append(input.charAt(i));
     }
+
+    boolean isPalindrome = input.contentEquals(reversed);
+
+    IO.println("Input text: " + input);
+    IO.println("Reversed text: " + reversed);
+    IO.println("Is it a Palindrome? : " + isPalindrome);
+}
